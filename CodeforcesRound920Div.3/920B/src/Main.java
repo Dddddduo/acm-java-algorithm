9c8f6d461c3ec5dedd94348ec9448ce090175c40
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2025-04-01 23:54:55 +0800

    Codeforces 920 div3

---
+++ CodeforcesRound920Div.3/920B/src/Main.java
@@ -0,0 +1,138 @@
+// @github https://github.com/Dddddduo
+// @github https://github.com/Dddddduo/acm-java-algorithm
+// @github https://github.com/Dddddduo/Dduo-mini-data_structure
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+import java.time.*;
+
+/**
+ * 题目地址
+ * https://codeforces.com/contest/1921/problem/B
+ */
+
+// xixi♡西
+public class Main {
+
+    static IoScanner sc = new IoScanner();
+    static final int mod = (int) (1e9 + 7);
+//    static final int mod = (int) (1e9 + 7);
+
+    static int n;
+    static int arr[];
+    static boolean visited[];
+    static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+    /**
+     * @throws IOException
+     */
+    private static void solve() throws IOException {
+        // todo
+        int n=sc.nextInt();
+        String str1=sc.next();
+        String str2=sc.next();
+
+        long cnt1=0;
+        long cnt2=0;
+
+        for (int i = 0; i < n; i++) {
+            if(str1.charAt(i)==str2.charAt(i)){
+                continue;
+            }else if(str1.charAt(i)=='1'&&str2.charAt(i)=='0'){
+                cnt1++;
+            }else if(str1.charAt(i)=='0'&&str2.charAt(i)=='1'){
+                cnt2++;
+            }
+        }
+
+        long cnt=0;
+        // cnt1<cnt2
+        if(cnt1>cnt2){
+            long ans=cnt1;
+            cnt1=cnt2;
+            cnt2=ans;
+        }
+//        dduoln(cnt1+" "+cnt2);
+        dduoln(Math.max(cnt1,cnt2));
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = 1;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    static <T> void dduo(T t) {
+        System.out.print(t);
+    }
+
+    static <T> void dduoln() {
+        System.out.println("");
+    }
+
+    static <T> void dduoln(T t) {
+        System.out.println(t);
+    }
+}
+
+/**
+ * IoScanner类
+ *
+ * @author Dduo
+ * @version 1.0
+ * @description 通过IO流操作缓冲区减少了与底层输入输出设备的交互次数，旨在简化 Java 中的标准输入读取操作。
+ */
+class IoScanner {
+    BufferedReader bf;
+    StringTokenizer st;
+    BufferedWriter bw;
+
+    public IoScanner() {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer("");
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public String nextLine() throws IOException {
+        return bf.readLine();
+    }
+
+    public String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    public char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    public float nextFloat() throws IOException {
+        return Float.parseFloat(next());
+    }
+
+    public BigInteger nextBigInteger() throws IOException {
+        return new BigInteger(next());
+    }
+
+    public BigDecimal nextDecimal() throws IOException {
+        return new BigDecimal(next());
+    }
+}
