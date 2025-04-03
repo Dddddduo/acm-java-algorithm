fd036a48ab89aab0dc27b7f4a588b3f7bbc272a9
Cute Puppy Dduo <164348579+Dddddduo@users.noreply.github.com>
2025-04-03 18:58:16 +0800

    热身

---
+++ Warm/warm1/src/Main.java
@@ -0,0 +1,137 @@
+// @github https://github.com/Dddddduo
+// @github https://github.com/Dddddduo/acm-java-algorithm
+// @github https://github.com/Dddddduo/Dduo-mini-data_structure
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+import java.time.*;
+
+/**
+ * 题目地址
+ *
+ */
+
+// xixi♡西
+public class Main {
+
+    static IoScanner sc = new IoScanner();
+    static final int mod = (int) (1e9 + 7);
+//    static final int mod = (int) (1e9 + 7);
+
+    static int n;
+    static int arr[];
+    static boolean visited[];
+    static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+    /**
+     * @throws IOException
+     */
+    private static void solve() throws IOException {
+        // todo
+        long n=sc.nextLong(); // 一共的天数
+        long d=sc.nextLong(); // 持续访问的时间
+
+        int k=sc.nextInt(); // 多少组工作时间
+        long arr[]=new long[(int) (n+1)];
+        for (int i = 0; i < k; i++) {
+            int l=sc.nextInt();
+            int r=sc.nextInt();
+            arr[l-1]++;
+            arr[r]--;
+        }
+
+        // 差分
+        long ans=0;
+        long a[]=new long[(int) n+1];
+        for (int i = 0; i < n + 1; i++) {
+            a[i]=ans;
+            ans+=arr[i];
+        }
+
+//        for (long l : a) {
+//            dduoln(l);
+//        }
+
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = 1;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+    }
+
+    static <T> void dduo(T t) {
+        System.out.print(t);
+    }
+
+    static <T> void dduoln() {
+        System.out.println("");
+    }
+
+    static <T> void dduoln(T t) {
+        System.out.println(t);
+    }
+}
+
+/**
+ * IoScanner类
+ *
+ * @author Dduo
+ * @version 1.0
+ * @description 通过IO流操作缓冲区减少了与底层输入输出设备的交互次数，旨在简化 Java 中的标准输入读取操作。
+ */
+class IoScanner {
+    BufferedReader bf;
+    StringTokenizer st;
+    BufferedWriter bw;
+
+    public IoScanner() {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer("");
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public String nextLine() throws IOException {
+        return bf.readLine();
+    }
+
+    public String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    public char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    public float nextFloat() throws IOException {
+        return Float.parseFloat(next());
+    }
+
+    public BigInteger nextBigInteger() throws IOException {
+        return new BigInteger(next());
+    }
+
+    public BigDecimal nextDecimal() throws IOException {
+        return new BigDecimal(next());
+    }
+}
