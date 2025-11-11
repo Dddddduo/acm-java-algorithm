d637552a93edb3176a072eb64f010e66fffa0c57
zhudaoyang <1732446549@qq.com>
2025-11-11 15:54:36 +0800

    codeforcesround1062

---
+++ CodeforcesRound1062Div.2/1062A/src/Main.java
@@ -0,0 +1,171 @@
+// https://github.com/Dddddduo/acm-java-algorithm
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+
+// 多多世界第一可爱!
+public class Main {
+
+    static IoScanner sc = new IoScanner();
+//    static final int mod = (int) (1e9 + 7);
+//    static final int mod = (int) (998244353);
+
+    static int n;
+    static int arr[];
+    static boolean visited[];
+    static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+    private static void solve() throws IOException {
+        int n = sc.nextInt();
+        int arr[]=new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=1;i<n-1;i+=2){
+            if(arr[i]!=arr[i+1]){
+                sc.println("NO");
+                return;
+            }
+        }
+
+        sc.println("YES");
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = 1;
+        t = sc.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        sc.flush();
+        sc.bw.close();
+    }
+
+}
+
+class IoScanner {
+    BufferedReader bf;
+    StringTokenizer st;
+    BufferedWriter bw;
+
+    public IoScanner() {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer("");
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    public String nextLine() throws IOException {
+        return bf.readLine();
+    }
+
+    public String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    public char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    public float nextFloat() throws IOException {
+        return Float.parseFloat(next());
+    }
+
+    public BigInteger nextBigInteger() throws IOException {
+        return new BigInteger(next());
+    }
+
+    public BigDecimal nextDecimal() throws IOException {
+        return new BigDecimal(next());
+    }
+
+    public void println(int a) throws IOException{
+        print(a);
+        println();
+    }
+
+    public void print(int a) throws IOException{
+        bw.write(String.valueOf(a));
+    }
+
+    public void println(String a) throws IOException{
+        print(a);
+        println();
+    }
+
+    public void print(String a) throws IOException{
+        bw.write(a);
+    }
+
+    public void println(long a) throws IOException{
+        print(a);
+        println();
+    }
+
+    public void print(long a) throws IOException{
+        bw.write(String.valueOf(a));
+    }
+
+    public void println(double a) throws IOException{
+        print(a);
+        println();
+    }
+
+    public void print(double a) throws IOException{
+        bw.write(String.valueOf(a));
+    }
+
+    public void print(BigInteger a) throws IOException{
+        bw.write(a.toString());
+    }
+
+    public void println(BigInteger a) throws IOException{
+        bw.write(a.toString());
+        println();
+    }
+
+    public void print(char a) throws IOException{
+        bw.write(String.valueOf(a));
+    }
+
+    public void println(char a) throws IOException{
+        print(a);
+        println();
+    }
+
+    public void println() throws IOException{
+        bw.newLine();
+    }
+
+    //其他调试命令：
+    public void flush() throws IOException{
+        //交互题分组调试，或者提前退出的情况下可以先运行此语句再推出
+        bw.flush();
+        return;
+    }
+
+    public boolean hasNext() throws IOException{
+        //本地普通IDE难以使用这个方法调试，需要按照数据组flush，刷新语句:
+        //sc.flush()
+        //调试完可删去
+        return bf.ready();
+    }
+
+}
