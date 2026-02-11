9fc7422d8393c02a0236bf61b1cc5fd0f48f8c5c
zhudaoyang <1732446549@qq.com>
2026-02-12 00:22:55 +0800

    1079Div2

---
+++ CodeforcesRound1079Div. 2/1079D/src/Main.java
@@ -0,0 +1,200 @@
+// https://github.com/Dddddduo/acm-java-algorithm
+// coding by Dduo from bhu-acm
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+import java.lang.*;
+
+// 多多世界第一可爱!
+// Dduo is the cutest girl in the world!
+public class Main {
+
+    private static DduoScanner sc = new DduoScanner();
+//    private static final long MOD = (long) (1e9 + 7);
+//    private static final long MOD = (long) (998244353);
+
+    private static int n;
+    private static int arr[];
+    private static boolean visited[];
+    private static ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+    private static Stack<Integer> stack = new Stack<>();
+    private static Queue<Integer> queue = new LinkedList<>();
+    private static Deque<Integer> deque = new LinkedList<>();
+    private static int dx[]={0,1,0,-1};
+    private static int dy[]={1,0,-1,0};
+
+    private static void solve() throws IOException {
+
+        int n = sc.nextInt();
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        long ans = 0;
+        int K = (int) Math.sqrt(n);
+
+        for (int i = 1; i <= n; i++) {
+
+            // 向前看
+            for (int x = 1; x <= K; x++) {
+                long dist = (long) x * arr[i];
+                long prev = i - dist;
+                if (prev < 1) break;
+                if (arr[(int)prev] == x) {
+                    ans++;
+                }
+            }
+
+            // 向后看
+            if (arr[i] > K) {
+                for (int y = 1; ; y++) {
+                    long j = i + (long) arr[i] * y;
+                    if (j > n) break;
+                    if (arr[(int)j] == y) {
+                        ans++;
+                    }
+                }
+            }
+        }
+
+        sc.println(ans);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int t = 1;
+        // 默认开启多组输入
+        t = sc.nextInt();
+        while (t-- > 0) {
+            solve();
+        }
+        sc.flush();
+        sc.bw.close();
+    }
+
+}
+
+class DduoScanner {
+    BufferedReader bf;
+    StringTokenizer st;
+    BufferedWriter bw;
+
+    public DduoScanner() {
+        bf = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer("");
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    }
+
+    protected String nextLine() throws IOException {
+        return bf.readLine();
+    }
+
+    protected String next() throws IOException {
+        while (!st.hasMoreTokens()) {
+            st = new StringTokenizer(bf.readLine());
+        }
+        return st.nextToken();
+    }
+
+    protected char nextChar() throws IOException {
+        return next().charAt(0);
+    }
+
+    protected int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    protected long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    protected double nextDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    protected float nextFloat() throws IOException {
+        return Float.parseFloat(next());
+    }
+
+    protected BigInteger nextBigInteger() throws IOException {
+        return new BigInteger(next());
+    }
+
+    protected BigDecimal nextDecimal() throws IOException {
+        return new BigDecimal(next());
+    }
+
+    protected void println(int a) throws IOException {
+        print(a);
+        println();
+    }
+
+    protected void print(int a) throws IOException {
+        bw.write(String.valueOf(a));
+    }
+
+    protected void println(String a) throws IOException {
+        print(a);
+        println();
+    }
+
+    protected void print(String a) throws IOException {
+        bw.write(a);
+    }
+
+    protected void println(long a) throws IOException {
+        print(a);
+        println();
+    }
+
+    protected void print(long a) throws IOException {
+        bw.write(String.valueOf(a));
+    }
+
+    protected void println(double a) throws IOException {
+        print(a);
+        println();
+    }
+
+    protected void print(double a) throws IOException {
+        bw.write(String.valueOf(a));
+    }
+
+    protected void print(BigInteger a) throws IOException {
+        bw.write(a.toString());
+    }
+
+    protected void println(BigInteger a) throws IOException {
+        bw.write(a.toString());
+        println();
+    }
+
+    protected void print(char a) throws IOException {
+        bw.write(String.valueOf(a));
+    }
+
+    protected void println(char a) throws IOException {
+        print(a);
+        println();
+    }
+
+    protected void println() throws IOException {
+        bw.newLine();
+    }
+
+    //其他调试命令：
+    protected void flush() throws IOException {
+        //交互题分组调试，或者提前退出的情况下可以先运行此语句再推出
+        bw.flush();
+        return;
+    }
+
+    protected boolean hasNext() throws IOException {
+        //本地普通IDE难以使用这个方法调试，需要按照数据组flush，刷新语句:
+        //sc.flush()
+        //调试完可删去
+        return bf.ready();
+    }
+
+}
